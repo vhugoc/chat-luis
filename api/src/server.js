1cc3fb7bfc5ce39d1c818d4602b0d24ebb02fc29
Victor Hugo <vhuugoc@gmail.com>
2020-07-06 15:08:34 -0300

    Configurações Iniciais

---
+++ api/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const router = require('./routes');
+const { database_auth } = require('./configs/credentials.json');
+
+const app = express();
+
+mongoose.connect(`mongodb+srv://${database_auth.user}:${database_auth.password}@${database_auth.uri}/test?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(router);
+
+app.listen(3030, () => {
+  console.log('Listening on port 3030');
+});
